b6d99cb543b0731f4f72e1e867af8528f503b6bf
Cleyson Barbosa <cleysonn@hotmail.com.br>
2024-10-09 02:01:54 -0300

    CHORE: add breadcrumbs on explore & catalog pages

--- src/app/(public)/catalog/page.tsx
+++ src/app/(public)/catalog/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link"
+
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
+import Stack from "@mui/material/Stack"
+import Breadcrumbs from "@mui/material/Breadcrumbs"
 
 import Header from "@/components/header"
 
@@ -7,6 +11,33 @@ export default function CatalogPage() {
   return (
     <Container>
       <Header nav={false} />
+
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        sx={{
+          color: "common.white",
+          p: "0.25rem",
+          alignSelf: "flex-start",
+        }}
+      >
+        <Stack
+          component={Link}
+          href="/"
+          sx={{
+            textDecoration: "underline",
+            transition: "color 0.5s ease",
+
+            "&:hover": {
+              transition: "color 0.5s ease",
+              color: "#FFFFFF80",
+            },
+          }}
+        >
+          Início
+        </Stack>
+
+        <Typography>Catálogo</Typography>
+      </Breadcrumbs>
 
       <Typography variant="h3" color="white">
         Catalog Animation
